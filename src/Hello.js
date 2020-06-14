ab4482c5bd4657043a76764a10117e52c573b531
Muhammad Moinuddin <54661456+Muhammed-Moinuddin@users.noreply.github.com>
2020-06-14 10:31:47 +0500

    Updated from Class 3

---
+++ src/Hello.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Hello.css'
+function Hello ({firstname}) {
+    return <p className = "favouritecolour"> This is a para in hello tag name ={firstname}</p>
+}
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><App name= "Moinuddin" age={25}/></div>,
   document.getElementById('root')
 );
 
