398316ad3afd61c2a55c62f0a9f31a329756b4d6
Muhammad Moinuddin <54661456+Muhammed-Moinuddin@users.noreply.github.com>
2020-06-17 17:43:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Hello from './Hello';
+import './aligning.css';
 
-function App({name,age}) {
-  return (
-      <div>
-      Hello from MoinsApp.js updated Name: {name} Age = {age - 10} 
-      <br />
-      <Hello firstname = {name}></Hello>
-      </div>
-  );
-}
+
+function App(props) {
+  return <div>
+   Hello From <strong className="text">{props.name} </strong>
+   <hr />
+   <div>My age is {props.age - 15}</div>
+   <div></div>
+   </div>
+} 
 
 export default App;
